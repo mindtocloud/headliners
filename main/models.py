bd49f6a5cf49f6b9d9d070bf6b7f6c8ee2728345
mindtocloud <nickelwick@outlook.com>
2017-10-15 17:50:42 +0100

    Updated

--- main/models.py
+++ main/models.py
@@ -7,4 +7,5 @@ class Article(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	url = models.URLField(max_length=250)
-	url_to_image = URLField(max_length=250)
+	url_to_image = models.URLField(max_length=250)
+
