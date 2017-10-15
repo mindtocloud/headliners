fcce2813d6d563e149427770182206683ea719a4
antonyCastieniras <antonycastineiras@gmail.com>
2017-10-15 18:18:54 +0100

    created article_params method

--- main/models.py
+++ main/models.py
@@ -7,4 +7,9 @@ class Article(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	url = models.URLField(max_length=250)
-	url_to_image = URLField(max_length=250)
+	url_to_image = models.URLField(max_length=250)
+	published_at = models.DateTimeField()
+
+def article_params(data):
+	return {'author': data['author'], 'title': data['title'], 'description': data['description'], 'url': data['url'], 'url_to_image': data['urlToImage'], 'published_at': data['publishedAt'] }
+
